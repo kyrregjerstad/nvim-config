3e936dc86d9609b77fda40f7a3d12dda1f1fb3ff
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-07-10 12:43:50 +0200

    update config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,11 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules", "%.git" },
+        },
+      })
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,15 @@ vim.cmd("set shiftwidth=2")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.g.background = "light"
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
